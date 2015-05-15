7c07012dfcd220a1c74d7a5479caa51cf07beb06
wardymate <chriswarddev@gmail.com>
2015-05-15 09:14:48 +0100

    all protractor tests passing

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,6 +1,28 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+    it('finds profiles', function() {
+    searchBox.sendKeys('wardymate');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.getText()).toContain('wardymate');
+  });
+
+  it('finds the last Spike', function() {
+    searchBox.sendKeys('wardymate');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('wardymate');
   });
 });
